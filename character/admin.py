9e4a65bcf58eda36db995c96adcec18fda54c699
Ian Hiddleston <ian.hid@gmail.com>
2018-11-25 20:41:01 +0000

    Initial draft of backend

--- character/admin.py
+++ character/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Branch, Player, Race, Nationality, Language, Guild, GuildRank, Character
 
 # Register your models here.
+admin.site.register(Branch)
+admin.site.register(Player)
+admin.site.register(Race)
+admin.site.register(Nationality)
+admin.site.register(Language)
+admin.site.register(Guild)
+admin.site.register(GuildRank)
+admin.site.register(Character)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Player
+    can_delete = False
+    verbose_name_plural = 'Player Profile'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- character/models.py
+++ character/models.py
@@ -1,3 +1,125 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class Branch(models.Model):
+    branch_code = models.CharField(max_length=10, primary_key=True, )
+    name = models.CharField(max_length=100, null=True)
+    lo_email = models.EmailField()
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. UK numbers are +44 and drop leading '0'. For example +441632962499. Up to 15 digits allowed.")
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, blank=True)
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    first_aider = models.BooleanField(default=False)
+    paid = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    next_kin_name = models.CharField(max_length=200)
+    next_kin_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    next_kin_relationship = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return (self.user.first_name + ' ' + self.user.last_name)
+    
+    class Meta:
+        ordering = ['user__first_name','user__last_name']
+
+    @receiver(post_save, sender=User)
+    def create_player_profile(sender, instance, created, **kwargs):
+        if created:
+            Player.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_player_profile(sender, instance, **kwargs):
+        instance.player.save()
+    
+    
+class Race(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    restricted = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+class Nationality(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    restricted = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+class Language(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    restricted = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+class Guild(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+    restricted = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+class GuildRank(models.Model):
+    name = models.CharField(max_length=100)
+    guild = models.ForeignKey(Guild, on_delete=models.CASCADE)
+    restricted = models.BooleanField()
+    
+    def __str__(self):
+        return ( self.guild.name + " " + self.name)
+    
+    class Meta:
+        ordering = ['guild__name', 'name',]
+    
+class Character(models.Model):
+    STATE_CHOICES = (
+        ('A', 'Active'),
+        ('D', 'Dead'),
+        ('R', 'Retired'),
+        ('N', 'NPC'),
+    )
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES)
+    started = models.DateTimeField(default=datetime.now)
+    resurrected = models.BooleanField()
+    excommunicated = models.BooleanField()
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    nationality = models.ForeignKey(Nationality, on_delete=models.CASCADE)
+    languages = models.ManyToManyField(Language)
+    guilds = models.ManyToManyField(Guild)
+    active = models.BooleanField()
+    notes = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return ( self.name )
+    
+    class Meta:
+        ordering = ['name',]
