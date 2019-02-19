88db890686960918e8a7a0467c82ed52a82a0f7f
Ian Hiddleston <ian.hid@gmail.com>
2019-02-19 16:32:18 +0000

    Update character files and templates, no user interface to speak of yet.

--- character/models.py
+++ character/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
-from django.core.validators import RegexValidator
-from datetime import datetime
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
+from django.utils.timezone import now
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -12,29 +12,30 @@ class Branch(models.Model):
     branch_code = models.CharField(max_length=10, primary_key=True, )
     name = models.CharField(max_length=100, null=True)
     lo_email = models.EmailField()
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         ordering = ('name',)
 
 
 class Player(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. UK numbers are +44 and drop leading '0'. For example +441632962499. Up to 15 digits allowed.")
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number should be entered in the format: '+xx9999999'. UK numbers are +44 and drop leading '0'. For example +441632962499. Up to 15 digits allowed.")
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, blank=True)
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
     first_aider = models.BooleanField(default=False)
     paid = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
-    next_kin_name = models.CharField(max_length=200)
-    next_kin_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
-    next_kin_relationship = models.CharField(max_length=200)
-    
+    registered = models.DateTimeField(default=now)
+    emergency_contact_name = models.CharField(max_length=200)
+    emergency_contact_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+    emergency_contact_relationship = models.CharField(max_length=200)
+
     def __str__(self):
         return (self.user.first_name + ' ' + self.user.last_name)
-    
+
     class Meta:
         ordering = ['user__first_name','user__last_name']
 
@@ -46,71 +47,96 @@ class Player(models.Model):
     @receiver(post_save, sender=User)
     def save_player_profile(sender, instance, **kwargs):
         instance.player.save()
-    
-    
+
+
 class Race(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     restricted = models.BooleanField()
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         ordering = ('name',)
 
 class Nationality(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     restricted = models.BooleanField()
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         ordering = ('name',)
 
 class Language(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     restricted = models.BooleanField()
-    
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         ordering = ('name',)
 
 class Guild(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     restricted = models.BooleanField()
-    
+    banned_races = models.ManyToManyField(Race, blank=True)
+
     def __str__(self):
         return self.name
-    
+
     class Meta:
         ordering = ('name',)
 
 class GuildRank(models.Model):
     name = models.CharField(max_length=100)
     guild = models.ForeignKey(Guild, on_delete=models.CASCADE)
+    starting_rank = models.BooleanField()
+    rank = models.PositiveSmallIntegerField()
     social_standing = models.PositiveIntegerField()
+    tithe_amount = models.IntegerField(null=True, blank=True)
+    tithe_percent = models.PositiveSmallIntegerField(null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(100)])
     restricted = models.BooleanField()
-    
+
     def __str__(self):
         return ( self.guild.name + " " + self.name)
-    
+
     class Meta:
-        ordering = ['guild__name', 'name',]
-    
+        ordering = ['guild__name', 'social_standing', 'rank']
+
+    def next_rank(self, guild, current_rank=None):
+        # Returns the next available guild rank.
+        # May be restricted so unable to advance without permission.
+        if current_rank is None:
+            return starting_rank(guild)
+        else:
+            #Find the next highest rank in the guild, this is the next one available.
+            #If maximum rank has been reached in the guild then return the current rank.
+            #May return a restricted rank, the player may not advance to this without permission.
+            return GuildRank.objects.filter(guild=guild, rank__gte=current_rank).order_by('rank')[:1]
+
+    def starting_rank(self, guild):
+        return GuildRank.objects.filter(guild=guild, starting_rank=True)
+
 class Character(models.Model):
+    ACTIVE = 'A'
+    DEAD = 'D'
+    RETIRED = 'R'
+    NPC = 'N'
+    MAX_GUILDS = 3
     STATE_CHOICES = (
-        ('A', 'Active'),
-        ('D', 'Dead'),
-        ('R', 'Retired'),
-        ('N', 'NPC'),
+        (ACTIVE, 'Active'),
+        (DEAD, 'Dead'),
+        (RETIRED, 'Retired'),
+        (NPC, 'NPC'),
     )
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     state = models.CharField(max_length=2, choices=STATE_CHOICES)
-    started = models.DateTimeField(default=datetime.now)
+    started = models.DateTimeField(default=now)
+    ended = models.DateTimeField(null=True, blank=True)
     resurrected = models.BooleanField()
     excommunicated = models.BooleanField()
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
@@ -119,9 +145,54 @@ class Character(models.Model):
     guilds = models.ManyToManyField(Guild)
     guild_ranks = models.ManyToManyField(GuildRank)
     notes = models.TextField(null=True, blank=True)
-    
+
     def __str__(self):
         return ( self.name )
-    
+
+    def is_active(self):
+        return self.status == ACTIVE
+
+    def is_dead(self):
+        return self.status == DEAD
+
+    def is_retired(self):
+        return self.status == RETIRED
+
+    def is_npc(self):
+        return self.status == NPC
+
+    def retire(self):
+        if self.is_active() :
+            self.state = RETIRED
+            self.ended = now()
+        else :
+            raise exception("Character is not active so cannot retire.")
+
+    def die(self):
+        if not self.is_dead():
+            self.state = DEAD
+            self.ended = now()
+        else:
+            raise exception("Character is already dead.")
+
+    def turn_npc(self):
+        if not self.is_dead():
+            self.state = NPC
+        else:
+            raise exception("Character is already dead.")
+        if self.ended == None:
+            self.ended = now()
+
+    def join_guild(self, guild):
+        character_guilds = self.guilds.all()
+        if len(character_guilds) < MAX_GUILDS:
+            self.guilds.add(guild)
+            self.guild_ranks.add(GuildRank.next_rank(guild))
+        else:
+            raise exception("Character a member of too many guilds: " + str(current_guilds))
+
+    def leave_guild(self, guild):
+        pass
+
     class Meta:
         ordering = ['name',]
--- character/views.py
+++ character/views.py
@@ -1,12 +1,28 @@
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Character, Player
+from .forms import SignUpForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'character/index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'character/signup.html', {'form': form})
 
 @login_required
 def player(request):
@@ -17,8 +33,8 @@ def list(request):
     #Get user Player details
     current_player = Player.objects.get(user=request.user)
     characters =  Character.objects.filter(player=current_player)
-    current_characters = characters.filter(state='A')
-    previous_characters = characters.exclude(state='A')
+    current_characters = characters.filter(state=Character.ACTIVE)
+    previous_characters = characters.exclude(state=Character.ACTIVE)
     context = {'current_characters': current_characters, 'previous_characters': previous_characters}
     return render(request, 'character/list.html', context)
 
